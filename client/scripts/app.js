d1d254a2c258364a5289c5e70fdb9eb75363aa93
myclamm <mikelam231@gmail.com>
2014-08-04 14:45:10 -0700

    installed jquery and underscore, implemented send and fetch methods on app

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,26 @@
 // YOUR CODE HERE:
+var app = {
+  init: function(){},
+  send: function(message){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      contentType: 'application/json',
+      type: 'POST',
+      data: JSON.stringify(message),
+      success: function(){console.log('message sent')},
+      error: function(){console.log('failed to send message')}
+    })
+  },
+  fetch: function(){
+    $.ajax({
+      url: this.server,
+      contentType: 'application/json',
+      type: 'GET',
+      success: function(data){console.dir(data)},
+      error: function(){console.log('Failed to receive message')}
+    });
+  },
+  server:'https://api.parse.com/1/classes/chatterbox'
+};
+
 
