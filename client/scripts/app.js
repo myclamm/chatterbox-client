2d247fc4d086271cf4787f0eb468e320f8b0da24
myclamm <mikelam231@gmail.com>
2014-08-04 17:07:10 -0700

    finished mocha specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,24 @@
 // YOUR CODE HERE:
+var username = prompt('Please choose a username');
+var room = 'lobby'
 var app = {
-  init: function(){},
+  init: function(){
+    $.ajax({
+      url: this.server,
+      contentType: 'application/json',
+      type: 'GET',
+      success: function(data){
+        app.clearMessages();
+        _.each(data.results,function(message){
+          app.addMessage(message);
+        }) ;
+      },
+      error: function(){console.log('Failed to receive message')}
+    });
+  },
   send: function(message){
     $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      url: this.server,
       contentType: 'application/json',
       type: 'POST',
       data: JSON.stringify(message),
@@ -12,15 +27,64 @@ var app = {
     })
   },
   fetch: function(){
+    var result;
     $.ajax({
       url: this.server,
       contentType: 'application/json',
       type: 'GET',
-      success: function(data){console.dir(data)},
+      success: function(data){
+        console.log(data.results);
+      },
       error: function(){console.log('Failed to receive message')}
     });
+    return result;
   },
-  server:'https://api.parse.com/1/classes/chatterbox'
+  server:'https://api.parse.com/1/classes/chatterbox',
+  clearMessages: function(){
+    $('#chats').empty();
+  },
+  addMessage: function(message){
+    // create li element
+    var $newItem = $("<li></li>");
+    // get data from message
+    // put data inside li
+    $($newItem).append('<div class="username">'+message.username+'</div');
+    $($newItem).append('<div class="msg">'+message.text+'</div');
+    $($newItem).append('<div class="room">'+message.roomname+'</div');
+    // append li to list
+    $('#chats').append($newItem);
+  },
+  addRoom: function(roomName){
+    //create an html object
+    var $newRoom = $('<option></option>');
+    $($newRoom).text(roomName);
+    //insert into roomselect
+    $('#roomSelect').append($newRoom);
+  },
+  addFriend: function(userName){
+    //add a class of friend to every username
+    //that matches the argument
+  },
+  handleSubmit: function(input){
+    var myMessage = {
+      username: username,
+      room: room,
+      text: input
+    };
+    app.send(myMessage);
+  }
 };
+$(document).ready(function(){
+  $('.submit').on('submit',function(e){
+    e.preventDefault();
+    var $message = $('#message');
+    app.handleSubmit($($message).val());
+    $($message).val('');
+  });
 
+  $('div').on('click','.username', function(){
+    app.addFriend($(this).text());
+  });
+  //setInterval(app.init.bind(app),5000);
+});
 
